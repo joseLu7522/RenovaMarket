736a039292718007ebe79ab235a2a7cffd01c381
joseLu7522 <jose.ls7522@gmail.com>
2024-04-15 21:45:26 +0200

    Footer y header hechos(falta enlaces)

---
+++ Proyecto2024/resources/views/partials/footer.blade.php
@@ -0,0 +1,45 @@
+<footer class="text-center text-lg-start">
+    <div class="container p-4">
+        <section>
+            <div class="row">
+                <div class="col-md-6 col-lg-6 col-xl-6 mx-auto mt-3">
+                    <h6 class="text-uppercase mb-4 font-weight-bold">RENOVAMARKET</h6>
+                    <p>
+                        "¡Bienvenido a RenovaMarket! Tu destino confiable para comprar y vender una amplia variedad de
+                        productos de segunda mano. Explora nuestro catálogo diverso y únete a una comunidad de
+                        compradores y vendedores apasionados. ¡Encuentra la tuya hoy!"
+                    </p>
+                </div>
+                <!--INFORMACIÓN DE CONTACTO-->
+                <div class="col-md-4 col-lg-4 col-xl-4 mx-auto mt-3">
+                    <h6 class="text-uppercase mb-4 font-weight-bold">CONTACTO</h6>
+                    <p><i class="bi bi-houses-fill"></i> Carrer els Ports, 8, 46940 Manises, Valencia</p>
+                    <p><i class="bi bi-envelope-at-fill"></i> renovaMarket@gmail.com</p>
+                    <p><i class="bi bi-telephone-fill"></i> 601 29 18 19</p>
+
+                </div>
+            </div>
+        </section>
+
+        <hr class="my-3">
+
+        <section class="p-3 pt-0">
+            <div class="row d-flex align-items-center">
+                <div class="col-md-7 col-lg-8 text-center text-md-start">
+                    <div class="p-3">
+                        © 2024 Jose Luis Sanchez Bosch
+                        <a class="text-white" href="https://mdbootstrap.com/">RenovaMarket</a>
+                    </div>
+                </div>
+                <!--ICONOS REDES SOCIALES-->
+                <div class="col-md-5 col-lg-4 ml-lg-0 text-center text-md-end">
+                    <a class="btn btn-outline-light btn-floating m-1" role="button"><i class="bi bi-facebook"></i></a>
+                    <a class="btn btn-outline-light btn-floating m-1" role="button"><i class="bi bi-twitter"></i></a>
+                    <a class="btn btn-outline-light btn-floating m-1" role="button"><i
+                            class="bi bi-envelope-at-fill"></i></a>
+                    <a class="btn btn-outline-light btn-floating m-1" role="button"><i class="bi bi-instagram"></i></a>
+                </div>
+            </div>
+        </section>
+    </div>
+</footer>
