f23cced465a21471d87aed273b60c7f08b96034f
joseLu7522 <jose.ls7522@gmail.com>
2024-04-18 23:33:55 +0200

    pull

--- Proyecto2024-JoseLuis/resources/views/layout.blade.php
+++ Proyecto2024-JoseLuis/resources/views/layout.blade.php
@@ -9,6 +9,9 @@
     <link rel="stylesheet" href="{{ asset('css/styles.css') }}">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
 </head>
 
--- Proyecto2024-JoseLuis/resources/views/partials/nav.blade.php
+++ Proyecto2024-JoseLuis/resources/views/partials/nav.blade.php
@@ -3,15 +3,16 @@
     <h1 class="title">RenovaMarket</h1>
 </header>
 
-<nav class="navbar navbar-expand-lg navbar-dark navbar-custom ">
-    <div class="container">
+<nav class="navbar navbar-expand-lg navbar-dark navbar-custom">
+    <div class="container-fluid mx-5">
         <a class="navbar-brand" href="{{ route('home') }}"><i class="bi bi-house-door-fill"></i> Inicio</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
             aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav ml-auto">
+
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                 <li class="nav-item active">
                     <a class="nav-link" href="{{ route('home') }}">Tienda Online</a>
                 </li>
@@ -44,6 +45,3 @@
     </div>
 </nav>
 
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
